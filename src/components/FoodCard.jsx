a624a3b0539a5fa563aa13e468f90d626b9979bb
KimKwon <khj9709@hanyang.ac.kr>
2021-11-21 02:22:54 +0900

    feat: :sparkles: FoodCard 추가

---
+++ src/components/FoodCard.jsx
@@ -0,0 +1,131 @@
+import styled, { css } from 'styled-components';
+
+import { ReactComponent as Coffee } from 'assets/img/coffee.svg';
+import { ReactComponent as Meal } from 'assets/img/meal.svg';
+
+function FoodCard(props) {
+  const { type } = props; // types = 'coffee' or 'meal'
+  return (
+    <StyledMenuSelection>
+      <LeftBox>
+        <CardWrapper>
+          {type === 'coffee' ? <Coffee /> : <Meal />}
+          <CardType>{type === 'coffee' ? '커피' : '식사'} </CardType>
+          <CardDesc>
+            <b>뭐뭐</b>때문에 <br />
+            <b>눈치보인다면?</b>
+          </CardDesc>
+        </CardWrapper>
+      </LeftBox>
+      <RightBox>
+        <InvertedBorder top />
+        <InvertedBorder bottom />
+        <CustomRadio type="radio" />
+      </RightBox>
+    </StyledMenuSelection>
+  );
+}
+
+const StyledMenuSelection = styled.article`
+  margin: 10rem;
+  display: flex;
+  width: 35rem;
+  height: 35rem;
+  border-radius: 8px;
+  background-color: white;
+  border: 1px solid #dddddd;
+`;
+
+const InvertedBorder = styled.i`
+  border: 1px solid #dddddd;
+  ${(props) =>
+    props.top
+      ? css`
+          border-top: none;
+        `
+      : css`
+          border-bottom: none;
+        `};
+  position: absolute;
+  width: 2rem;
+  height: 1rem;
+
+  ${(props) =>
+    props.bottom
+      ? css`
+          border-radius: 1rem 1rem 0 0;
+        `
+      : css`
+          border-radius: 0 0 1rem 1rem;
+        `};
+
+  background-color: white;
+  ${(props) => (props.bottom ? 'bottom : 0' : 'top: 0')};
+  left: 0;
+  ${(props) =>
+    props.bottom
+      ? css`
+          transform: translateX(-50%);
+        `
+      : css`
+          transform: translateX(-50%);
+        `}
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+
+  & > svg {
+    width: 10rem;
+    height: 10rem;
+    margin-bottom: 3rem;
+  }
+`;
+
+const CardType = styled.h3`
+  background-color: black;
+  color: white;
+  width: fit-content;
+  padding: 0.5rem;
+  margin-bottom: 2rem;
+
+  font-size: 1.8rem;
+  line-height: 2.16rem;
+  letter-spacing: -1%;
+  font-weight: 500;
+
+  &:before {
+    content: '# ';
+  }
+`;
+
+const CardDesc = styled.p`
+  font-size: 2.6rem;
+  line-height: 145%;
+  letter-spacing: -0.01rem;
+  & b {
+    font-weight: bolder;
+  }
+`;
+
+const LeftBox = styled.div`
+  flex: 5;
+  display: flex;
+  justify-content: flex-start;
+
+  border-right: 1px dashed #dddddd;
+`;
+const RightBox = styled.div`
+  position: relative;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CustomRadio = styled.input``;
+
+export default FoodCard;
--- src/components/MenuSelection.jsx
+++
@@ -1,5 +0,0 @@
-function MenuSelection() {
-  return <div></div>;
-}
-
-export default MenuSelection;
