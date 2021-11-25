5bdf1d4883ae6b62efdfe9a7d0c28a1ec723dec6
younyikim <rlakuku1221@gmail.com>
2021-11-25 16:47:08 +0900

    :hammer: refactor : CartModal 중복 스타일 제거 등

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,18 +1,121 @@
 import styled from 'styled-components';
+import FoodCartCard from 'components/FoodCartCard';
 
-function CartModal() {
-  return <StyledCartWrapper></StyledCartWrapper>;
+function CartModal({ coEatList, noEatList, setIsOpen }) {
+  return (
+    <StyledCartWrapper>
+      <CloseButton onClick={() => setIsOpen(false)}>X</CloseButton>
+      <StyledListWrapper color="COEAT">
+        <div>
+          <StyledTitle>
+            <span>COEAT</span>
+            <StyledUnderLine color="COEAT" />
+          </StyledTitle>
+          <span>{Object.keys(coEatList).length}</span>
+        </div>
+        <ul>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+        </ul>
+      </StyledListWrapper>
+      <StyledListWrapper>
+        <div>
+          <StyledTitle>
+            <span>NOEAT</span>
+            <StyledUnderLine color="NOEAT" />
+          </StyledTitle>
+          <span>{Object.keys(noEatList).length}</span>
+        </div>
+        <ul>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+          <li>
+            <FoodCartCard />
+          </li>
+        </ul>
+      </StyledListWrapper>
+    </StyledCartWrapper>
+  );
 }
 
 export default CartModal;
 
 const StyledCartWrapper = styled.div`
-  overflow: auto;
+  display: flex;
   position: absolute;
   bottom: 9.2rem;
   height: 68.9rem;
   width: 100%;
-  font-size: 8rem;
   background-color: #f4f5f6;
+  font-size: 8rem;
   color: black;
+  overflow: scroll;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+  border: 0;
+  background-color: transparent;
+`;
+
+const StyledTitle = styled.header`
+  display: flex;
+  flex-direction: column;
+  font-size: 2.8rem;
+  line-height: 2.2rem;
+  margin-right: 2.1rem;
+  color: black;
+`;
+
+const StyledUnderLine = styled.div`
+  width: 10rem;
+  height: 0.8rem;
+  background: ${(prop) => (prop.color == 'COEAT' ? '#ff912d' : 'black')};
+  margin-top: 0.4rem;
+`;
+
+const StyledListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1 1 50%;
+  width: 30%;
+  background-color: #f4f5f6;
+  border-right: 1px solid #e6e6e6;
+
+  & > div {
+    display: flex;
+    font-size: 2.4rem;
+    text-align: center;
+    margin-top: 4.6rem;
+    color: ${(prop) => (prop.color == 'COEAT' ? '#ff912d' : 'black')};
+  }
 `;
--- src/components/PickCartNav.jsx
+++ src/components/PickCartNav.jsx
@@ -1,18 +1,14 @@
 import styled from 'styled-components';
 import { ReactComponent as ProfileIcon } from 'assets/profile.svg';
 import { ReactComponent as CartlBtnIcon } from 'assets/cartlBtn.svg';
-import FoodCartCard from 'components/FoodCartCard';
 import CartModal from 'components/CartModal';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function PickCartModal({ coEatList, noEatList, isOpen, setIsOpen }) {
   const navigator = useNavigate();
-  //   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
-    console.log('clicked');
   };
 
   const getTotalEatList = (list) => {
@@ -25,58 +21,8 @@ function PickCartModal({ coEatList, noEatList, isOpen, setIsOpen }) {
 
   return (
     <>
-      {/* <StyledCartModalWrapper isOpen={isOpen}>
-        <CloseButton onClick={() => setIsOpen(false)}>X</CloseButton>
-        <StyledCoEat>
-          <div>
-            <div>
-              <span>COEAT</span>
-              <div></div>
-            </div>
-            <span>{Object.keys(coEatList).length}</span>
-          </div>
-          <ul>
-            <li>
-              <FoodCartCard />
-            </li>
-            <li>
-              <FoodCartCard />
-            </li>
-            <li>
-              <FoodCartCard />
-            </li>
-            <li>
-              <FoodCartCard />
-            </li>
-          </ul>
-        </StyledCoEat>
-        <StyledNoEat>
-          <div>
-            <div>
-              <span>NOEAT</span>
-              <div></div>
-            </div>
-            <span>{Object.keys(noEatList).length}</span>
-          </div>
-          <ul>
-            <li>
-              <FoodCartCard />
-            </li>
-            <li>
-              <FoodCartCard />
-            </li>
-            <li>
-              <FoodCartCard />
-            </li>
-            <li>
-              <FoodCartCard />
-            </li>
-          </ul>
-        </StyledNoEat>
-      </StyledCartModalWrapper> */}
-
       <StyledCartNavWrapper>
-        {isOpen && <CartModal />}
+        {isOpen && <CartModal coEatList={coEatList} noEatList={noEatList} setIsOpen={setIsOpen} />}
         <StyledCartNav>
           <StyledUserProfile>
             <ProfileIcon />
@@ -105,14 +51,6 @@ function PickCartModal({ coEatList, noEatList, isOpen, setIsOpen }) {
 }
 
 export default PickCartModal;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 1rem;
-  right: 1rem;
-  border: 0;
-  background-color: transparent;
-`;
 
 const StyledCartNavWrapper = styled.div`
   display: flex;
@@ -188,82 +126,4 @@ const StyledResultBtn = styled.button`
   width: 18em;
   height: 4.9rem;
   font-weight: bolder;
-`;
-
-const StyledCartModalWrapper = styled.div`
-  position: fixed;
-  bottom: 9.2rem;
-  display: ${(prop) => (prop.isOpen ? 'flex' : 'none')};
-  justify-content: center;
-  margin: 0 auto;
-  width: 100%;
-  height: 77rem;
-  background-color: #f4f5f6;
-`;
-
-const StyledCoEat = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 30%;
-  overflow-y: hidden;
-  background-color: #f4f5f6;
-  border-right: 1px solid #e6e6e6;
-
-  & > div {
-    display: flex;
-    font-size: 2.4rem;
-    text-align: center;
-    margin-top: 4.6rem;
-    color: #ff912d;
-
-    & > div {
-      display: flex;
-      flex-direction: column;
-      font-size: 2.8rem;
-      line-height: 2.2rem;
-      margin-right: 2.1rem;
-      color: black;
-
-      & > div {
-        width: 10rem;
-        height: 0.8rem;
-        background: #ff912d;
-        margin-top: 0.4rem;
-      }
-    }
-  }
-`;
-
-const StyledNoEat = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 30%;
-  overflow-y: hidden;
-  background-color: #f4f5f6;
-
-  & > div {
-    display: flex;
-    font-size: 2.4rem;
-    text-align: center;
-    margin-top: 4.6rem;
-    color: #000;
-
-    & > div {
-      display: flex;
-      flex-direction: column;
-      font-size: 2.8rem;
-      line-height: 2.2rem;
-      margin-right: 2.1rem;
-      color: black;
-
-      & > div {
-        width: 10rem;
-        height: 0.8rem;
-        background: #000;
-        margin-top: 0.4rem;
-      }
-    }
-  }
 `;
