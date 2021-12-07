4bc3d0ab5673fb07aeedac8bbaf1bf6b33860004
younyikim <rlakuku1221@gmail.com>
2021-12-07 11:45:31 +0900

    :tada: feat : mainPage - 방장 UI 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,179 +1,218 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-import Logo from 'assets/logo.svg';
-import LogoPic from 'assets/logo-pic.svg';
-import Arrow from 'assets/arrow.png';
+import { ReactComponent as Coeat } from 'assets/logo.svg';
+import { ReactComponent as Sticker } from 'assets/sticker.svg';
+import { ReactComponent as Clip } from 'assets/clip.svg';
+import { ReactComponent as RiceIcon } from 'assets/rice.svg';
 import { ReactComponent as GoIcon } from 'assets/go.svg';
-import EntryFoodCard from 'components/EntryFoodCard';
-import { useNavigate } from 'react-router';
-import { colors } from 'constants/colors';
 
 function MainPage() {
-  const [isFocus, setIsFocus] = useState(false);
-  const [selectedCard, setSelectedCard] = useState(''); // meal or coffee
-  const [user, setUser] = useState('');
-  const navigator = useNavigate();
-
-  const handleChange = (e) => {
-    setUser(e.target.value);
-  };
-
-  const handleAdd = () => {
-    if (!user) return;
-    window.localStorage.setItem('user', user);
-    setUser('');
-    navigator('pick', { state: { selectedCard } });
-  };
-
   return (
     <StyledContainer>
-      <img className="logo_img" src={Logo} alt="logo" />
-      <img className="logo_pic_img1" src={LogoPic} alt="logoPic1" />
-      <img className="logo_pic_img2" src={LogoPic} alt="logoPic2" />
-      <img className="arrow_img" src={Arrow} alt="arrow" />
-      <div className="title">
-        <span>함께 정하는 우리 메뉴</span>
-      </div>
-
-      <CardWrapper>
-        <EntryFoodCard type="meal" selectedCard={selectedCard} setSelectedCard={setSelectedCard} />
-        <EntryFoodCard type="coffee" selectedCard={selectedCard} setSelectedCard={setSelectedCard} />
-      </CardWrapper>
-      <StyledInput isFocus={isFocus}>
-        <p>사용하실 닉네임을 입력해주세요.</p>
-        <input
-          type="text"
-          placeholder="닉네임 입력"
-          onFocus={() => setIsFocus(true)}
-          onBlur={() => setIsFocus(false)}
-          value={user}
-          onChange={handleChange}
-        />
-      </StyledInput>
-      <StyledButton>
+      <StyledMainHeader>
+        <StyledTitle>
+          <Sticker />
+          <Coeat />
+        </StyledTitle>
+        <StyledContent>
+          <p>우리 오늘 뭐 먹을래?</p>
+          <p>이젠, 코잇으로 정해보세요</p>
+        </StyledContent>
+      </StyledMainHeader>
+      <StyledMainBody>
+        <Clip />
+        <StyledBodyContent>
+          <StyledBodyTitle>
+            <RiceIcon />
+            <span>HOW TO USE COEAT?</span>
+          </StyledBodyTitle>
+          <StyledBodyDesc>
+            <StyledCoeat>
+              <StyledLeft>
+                <p>
+                  오늘은 이 메뉴가 <em>먹고 싶다면?</em>
+                </p>
+                <p className="subtitle">최대 5개까지 코잇!</p>
+              </StyledLeft>
+              <StyledRight>COEAT</StyledRight>
+            </StyledCoeat>
+            <StyledCoeat>
+              <StyledLeft>
+                <p>
+                  오늘 이 메뉴만큼은 <em>피하고 싶다면?</em>
+                </p>
+                <p className="subtitle">최대 5개까지 노잇!</p>
+              </StyledLeft>
+              <StyledRight noeat>NOEAT</StyledRight>
+            </StyledCoeat>
+          </StyledBodyDesc>
+        </StyledBodyContent>
+      </StyledMainBody>
+      <StyledMainButton>
+        <div>
+          <span>방 만들기</span>
+        </div>
         <GoIcon />
-        <button onClick={handleAdd}>Let’s COEAT!</button>
-      </StyledButton>
+      </StyledMainButton>
     </StyledContainer>
   );
 }
 
-const StyledContainer = styled.div`
-  width: 100%;
+export default MainPage;
+
+const StyledContainer = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
   align-items: center;
+  justify-content: space-between;
+  width: 40%;
+  margin: 0 auto;
   height: 100%;
-  position: relative;
+`;
 
-  .title {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    top: 19.9rem;
-    left: 50%;
-    transform: translate(-50%);
-  }
-  .title span {
-    font-size: 2.8rem;
-  }
-  .logo_img,
-  .logo_pic_img1,
-  .logo_pic_img2,
-  .arrow_img {
-    position: absolute;
-    transform: translate(-50%);
-  }
-  .logo_img {
-    width: 20.9rem;
-    height: 7.1rem;
-    top: 10.3rem;
-    left: 50%;
-  }
+const StyledMainHeader = styled.header`
+  margin-top: 10rem;
+`;
 
-  .logo_pic_img1 {
-    width: 11.898rem;
-    height: 11.898rem;
-    top: 16.536rem;
-    left: 33.814rem;
-  }
+const StyledTitle = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 8rem;
+  margin-bottom: 4.2rem;
 
-  .logo_pic_img2 {
-    width: 16.291rem;
-    height: 16.291rem;
-    top: 3.579rem;
-    right: 33.043rem;
-  }
-
-  .arrow_img {
-    width: 7.1rem;
-    height: 7.1rem;
-    top: 12.9rem;
-    left: 55.4rem;
+  & > svg:first-child {
+    width: 15rem;
+    height: 15rem;
   }
 `;
 
-const StyledInput = styled.div`
+const StyledContent = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
+  align-items: center;
 
   & > p {
-    font-size: 2rem;
-    margin-top: 5.939rem;
-    margin-bottom: 1.8rem;
+    font-size: 3rem;
+    font-weight: 400;
+    line-height: 4.5rem;
   }
 
-  & > input {
-    width: 50rem;
-    height: 6.4rem;
-    border: ${(prop) => (prop.isFocus ? `1px solid ${colors.darkOrange}` : 0)};
-    border-radius: 1.2rem;
-    outline: 0;
-    background-color: ${(prop) => (prop.isFocus ? colors.white : colors.gray)};
-    padding: 2rem 2.5rem;
-    margin-bottom: 8.4rem;
-    font-size: 2rem;
-    color: ${(prop) => (prop.isFocus ? colors.darkOrange : colors.grayText)};
-  }
-
-  & > input::placeholder {
-    font-size: 2rem;
-    color: ${(prop) => (prop.isFocus ? colors.darkOrange : colors.grayText)};
+  & > p:last-child {
+    font-weight: 700;
   }
 `;
 
-const StyledButton = styled.div`
+const StyledMainBody = styled.article`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  margin-top: 4rem;
   position: relative;
+  width: 100%;
 
-  & > button {
-    border: 0;
-    border-radius: 0.2rem;
-    outline: 0;
-    width: 28.1rem;
-    height: 7.8rem;
-    background-color: ${colors.darkOrange};
-    margin-bottom: 9.8rem;
-    font-size: 2.4rem;
+  & > svg {
+    position: relative;
+    top: 1rem;
+  }
+`;
+
+const StyledBodyContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 68rem;
+  border: 1.5rem solid #f5f5f5;
+  border-radius: 4rem;
+`;
+
+const StyledBodyTitle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 6rem;
+
+  & > svg {
+    width: 8rem;
+    height: 8rem;
+    margin-right: 2.5rem;
+  }
+
+  & > span {
+    font-size: 3rem;
     font-weight: 700;
+    margin-right: 10rem;
+  }
+`;
+
+const StyledBodyDesc = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 6.1rem;
+`;
+
+const StyledCoeat = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 4.3rem;
+
+  font-size: 2.4rem;
+  font-weight: 400;
+  line-height: 2.8rem;
+`;
+
+const StyledLeft = styled.div`
+  margin-right: 7.4rem;
+  .subtitle {
+    font-size: 1.8rem;
+    color: #888;
+  }
+
+  & > p {
+    & > em {
+      font-weight: 700;
+    }
+  }
+`;
+
+const StyledRight = styled.div`
+  width: 16rem;
+  height: 7.3rem;
+  background-color: ${(props) => (props.noeat ? '#F5F5F5' : '#ffefe0')};
+  color: ${(props) => (props.noeat ? '#888888' : '#ff7a00')};
+  padding: 2.5rem 4.8rem;
+  font-weight: 700;
+  position: relative;
+  top: -1rem;
+`;
+
+const StyledMainButton = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  margin-top: 6rem;
+  margin-bottom: 11rem;
+
+  & > div {
+    display: flex;
+    align-items: center;
+    width: 28rem;
+    height: 7.8rem;
+    padding: 2.4rem 9.3rem;
+    background-color: #ff7a00;
+
+    & > span {
+      font-size: 2.4rem;
+      font-weight: 700;
+      color: white;
+    }
   }
 
   & > svg {
     position: absolute;
-    top: -4rem;
+    top: -5rem;
     right: -4rem;
-    transform: rotate(10deg);
+    width: 10rem;
+    height: 10rem;
   }
 `;
-
-const CardWrapper = styled.div`
-  display: flex;
-  gap: 2rem;
-`;
-
-export default MainPage;
