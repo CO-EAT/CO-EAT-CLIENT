62f31ab669b96014cbabf29101c4b6639dc56304
younyikim <rlakuku1221@gmail.com>
2021-12-07 17:52:44 +0900

    :fire: edit : 색종이 이미지 추가, index.js 작성

---
+++ src/assets/img/index.js
@@ -0,0 +1,22 @@
+import paper from 'assets/img/papers/paper.png';
+import paper1 from 'assets/img/papers/paper1.png';
+import paper2 from 'assets/img/papers/paper2.png';
+import paper3 from 'assets/img/papers/paper3.png';
+import paper4 from 'assets/img/papers/paper4.png';
+import paper5 from 'assets/img/papers/paper5.png';
+import paper6 from 'assets/img/papers/paper6.png';
+import paper7 from 'assets/img/papers/paper7.png';
+import paper8 from 'assets/img/papers/paper8.png';
+
+const papers = {
+  paper,
+  paper1,
+  paper2,
+  paper3,
+  paper4,
+  paper5,
+  paper6,
+  paper7,
+  paper8,
+};
+export default papers;
